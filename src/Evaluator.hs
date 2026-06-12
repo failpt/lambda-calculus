479d612125606498df9e2bde12c8fb34fcb177fb
Uliana Pryshedko <knopkmop@gmail.com>
2026-06-12 21:46:42 +0200

    Fix print format

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -7,7 +7,8 @@ data Term = Var String
 instance Show Term where
     show (Var name) = name
     show (Abs arg body) = "\\" ++ arg ++ ". " ++ show body
-    show (App t1 t2) = "(" ++ show t1 ++ " " ++ show t2 ++ ")"
+    show (App l (Var name)) = show l ++ " " ++ name
+    show (App l r) = show l ++ " (" ++ show r ++ ")"
 
 type Venv = [(String, Term)]
 
@@ -17,34 +18,32 @@ eval v (Var name) = case lookup name v of
         Just t -> eval v t
         Nothing -> Var name
 eval v (Abs arg body) = Abs arg body
-eval v (App t1 t2) = case eval v t1 of
-        Abs arg body -> eval v $ reduce arg body t2
-        term -> App term $ eval v t2
+eval v (App l r) = case eval v l of
+        Abs arg body -> eval v $ reduce arg body r
+        term -> App term $ eval v r
         
 isFree :: String -> Term -> Bool
 -- | Returns True if a variable is free in a term, otherwise returns False.
 isFree x (Var name) = x == name 
-isFree x (Abs arg body)
-    | x == arg = False
-    | otherwise = isFree x body
-isFree x (App t1 t2) = isFree x t1 || isFree x t2
+isFree x (Abs arg body) = x /= arg && isFree x body
+isFree x (App l r) = isFree x l || isFree x r
 
 reduce :: String -> Term -> Term -> Term
 -- | Substitudes a variable everywhere in a term with a different (input) term and returns the result.
-reduce arg (Var name) input
-    | arg == name = input
+reduce x (Var name) t
+    | x == name = t
     | otherwise = Var name
-reduce arg1 (Abs arg2 body) input
-    | arg1 == arg2 = Abs arg2 body
-    | isFree arg2 input = let arg2' = arg2 ++ "'" in Abs arg2' $ reduce arg1 (reduce arg2 body $ Var arg2') input  
-    | otherwise = Abs arg2 $ reduce arg1 body input
-reduce arg (App t1 t2) input = App (reduce arg t1 input) (reduce arg t2 input)
+reduce x (Abs arg body) t
+    | x == arg = Abs arg body
+    | isFree arg t = let arg' = arg ++ "'" in Abs arg' $ reduce arg (reduce arg body $ Var arg') t  
+    | otherwise = Abs arg $ reduce x body t
+reduce x (App l r) t = App (reduce x l t) (reduce x r t)
 
 eta :: Venv -> Term -> Term
 -- | Eta reduces a term after evaluation.
 eta v t = case eval v t of
     Var name -> Var name 
-    App t1 t2 -> App (eta v t1) (eta v t2)
+    App l r -> App (eta v l) (eta v r)
     Abs arg body -> case eta (filter (\(x, _) -> x /= arg) v) body of
         App f (Var x) | x == arg && not (isFree x f) -> f
         body' -> Abs arg body'
